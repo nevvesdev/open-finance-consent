9a7996834e95608533247d6d75d7ec33209957b1
João Victor <joaodddev@gmail.com>
2026-09-03 19:22:34 -0300

    chore: setup inicial do projeto

---
+++ src/main/java/br/com/nevvesdev/openfinance/OpenFinanceConsentApplication.java
@@ -0,0 +1,13 @@
+package br.com.nevvesdev.openfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFinanceConsentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFinanceConsentApplication.class, args);
+	}
+
+}
